f323344a4f77b18df8c99ba61125151f36ca17e9
Aaron Li <92602862+xingmeizhi@users.noreply.github.com>
2024-01-29 01:02:05 -0500

    Player controller

--- Game3400/Assets/Project1/Script/PlayerController.cs
+++ Game3400/Assets/Project1/Script/PlayerController.cs
@@ -4,58 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float moveSpeed = 10;
+    public float moveSpeed = 3f;
     public float gravity = 9.81f;
-    public float jumpHeight = 10;
-    public float airControl = 10;
+    public float jumpHeight = 0.3f;
+    public float maxJumpHeight = 1f;
+    public float airControl = 1f;
+
+    private float jumpPressDuration = 0f;
+    private bool isPreparingToJump = false;
 
     CharacterController controller;
     Vector3 input, moveDirection;
-    // Start is called before the first frame update
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+        input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+        input *= moveSpeed;
 
-            input = (transform.right * moveHorizontal + transform.forward * moveVertical).normalized;
+        if (controller.isGrounded)
+        {
+            moveDirection = input;
 
-
-            input *= moveSpeed;
-
-            if (controller.isGrounded)
+            if (Input.GetButtonDown("Jump"))
             {
-                moveDirection = input;
-
-                if (Input.GetButton("Jump"))
-                {
-                    moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
-                }
-                else
-                {
-                    moveDirection.y = 0.0f;
-                }
-            }
-            else
-            {
-                input.y = moveDirection.y;
-                moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+                isPreparingToJump = true;
+                jumpPressDuration = 0f;
             }
 
-            moveDirection.y -= gravity * Time.deltaTime;
+            if (isPreparingToJump && Input.GetButtonUp("Jump"))
+            {
+                isPreparingToJump = false;
+                float scaledJumpHeight = Mathf.SmoothStep(jumpHeight, maxJumpHeight, Mathf.Sqrt(jumpPressDuration));
+                moveDirection.y = Mathf.Sqrt(2 * scaledJumpHeight * gravity);
+            }
 
-            controller.Move(moveDirection * Time.deltaTime);
+            if (isPreparingToJump)
+            {
+                jumpPressDuration += Time.deltaTime;
+                jumpPressDuration = Mathf.Clamp(jumpPressDuration, 0f, 1f);
+            }
+        }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
 
-
-
-
-
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
